45fb1e627865d09a37072c7f010c738c880b4a61
Harry Barber <harrybarber@protonmail.com>
2020-04-04 18:16:47 +0100

    Improve UI startup procedure

--- mlb-ui/src/main.rs
+++ mlb-ui/src/main.rs
@@ -1,7 +1,6 @@
-extern crate sdl2;
-
 use client::{types::ThumbnailData, MlbClient};
 
+use futures::prelude::*;
 use parking_lot::RwLock;
 use sdl2::{
     event::Event,
@@ -13,9 +12,10 @@ use std::{collections::HashMap, path::Path, sync::Arc};
 
 const BACKGROUND_PATH: &str = "./assets/background.jpg";
 
+#[derive(Debug)]
 pub enum MainState {
     FetchingJson,
-    FetchingImages(Vec<ThumbnailData>, HashMap<usize, Vec<u8>>),
+    FetchingImages(Vec<ThumbnailData>, HashMap<usize, Result<Vec<u8>, String>>),
     Error,
 }
 
@@ -23,21 +23,56 @@ async fn startup_procedure(client: MlbClient, main_state: Arc<RwLock<MainState>>
     let example_date = time::date!(2018 - 06 - 10);
     match client.get_schedule_via_date(&example_date).await {
         Err(err) => {
+            // Reached error state - no thumbnail data found
             *main_state.write() = MainState::Error;
             return;
         }
         Ok(schedule) => {
+            // Collect thumbnail data
             let mut thumbnail_data = schedule.into_thumbnail_data();
             let thumbnails = match thumbnail_data.pop() {
                 Some(some) => some,
                 None => {
+                    // Reached error state - no thumbnail data found
                     *main_state.write() = MainState::Error;
                     return;
                 }
             };
+
+            // Collect image URLs
+            let image_urls: Vec<Option<String>> = thumbnails
+                .iter()
+                .map(move |thumbnail| thumbnail.photos.get("684x385").cloned())
+                .collect();
+
             let image_map = HashMap::with_capacity(thumbnails.len());
             *main_state.write() = MainState::FetchingImages(thumbnails, image_map);
-            println!("{:?}", thumbnail_data);
+
+            // Join all image fetching futures
+            let image_fetching = future::join_all(image_urls.iter().enumerate().map(|(i, url)| {
+                println!("{:?}", url);
+                let client_inner = client.clone();
+                let main_state_inner = main_state.clone();
+                async move {
+                    let image_raw = if let Some(url) = url {
+                        client_inner
+                            .get_image(url)
+                            .await
+                            .map_err(|err| err.to_string())
+                    } else {
+                        Err("URL not found".to_string())
+                    };
+
+                    // If in fetching images state then insert image
+                    match &mut *main_state_inner.write() {
+                        MainState::FetchingImages(_, image_map) => {
+                            image_map.insert(i, image_raw);
+                        }
+                        _ => (),
+                    }
+                }
+            }));
+            image_fetching.await;
         }
     }
 }
